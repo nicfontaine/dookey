3048f19a24d49607732d712644a8d1fdf2e00a0c
nicfontaine <nic@nicfontaine.com>
2023-05-08 12:12:43 -0700

    Grid layout fix for command options

--- src/components/EntryCommandOptions.jsx
+++ src/components/EntryCommandOptions.jsx
@@ -37,62 +37,67 @@ const EntryCommandOptions = ({
 					</div>
 
 					<h2 className="section-heading"><span className="mg-r-1">🏭</span> Functions</h2>
-					<div className="d-flx flx-children-1 flx-spc-2 flx-children-spc-2">
+					<div className="group-parent-2 d-flx flx-children-1 flx-spc-2 flx-children-spc-2">
 						<div className="group d-flx flx-row">
-							<div>
-								<div className="row"><span className="code">/export</span></div>
-								<div className="row"><span className="code">/import</span></div>
-								<div className="row"><span className="code">/save</span></div>
-								<div className="row"><span className="code">/open</span></div>
-							</div>
-							<div className="desc">
-								<div className="row">Copy data to clipboard</div>
-								<div className="row">Paste &amp; merge data</div>
-								<div className="row">Save to local backup file</div>
-								<div className="row">Load from backup file</div>
-							</div>
+							<div className="row"><span className="code">/export</span></div>
+							<div className="row desc">Copy data to clipboard</div>
+							<div className="row"><span className="code">/import</span></div>
+							<div className="row desc">Paste &amp; merge data</div>
+							<div className="row"><span className="code">/save</span></div>
+							<div className="row desc">Save to local file</div>
+							<div className="row"><span className="code">/open</span></div>
+							<div className="row desc">Load from local file</div>
 						</div>
 						<div className="group d-flx flx-row">
-							<div>
-								<div className="row"><span className="code">/nuke</span></div>
-								<div className="row"><span className="code">/help</span></div>
-								<div className="row"><span className="code">/clock</span></div>
-							</div>
-							<div className="desc">
-								<div className="row">Delete all notes</div>
-								<div className="row">More info in README</div>
-								<div className="row">Display a clock overlay</div>
-							</div>
+							<div className="row"><span className="code">/nuke</span></div>
+							<div className="row desc">Delete all notes</div>
+							<div className="row"><span className="code">/help</span></div>
+							<div className="row desc">More info in README</div>
+							<div className="row"><span className="code">/clock</span></div>
+							<div className="row desc">Clock overlay</div>
 						</div>
 					</div>
+
 					<h2 className="section-heading"><span className="mg-r-1">⚙️</span> Settings</h2>
+					
 					<div className="group d-flx flx-row">
-						<div>
-							<div className="row"><span className="code">/title &lt;text&gt;</span></div>
-							<div className="row"><span className="code">/size {settings.fontSize || "<number>"}</span></div>
-							<div className="row"><span className="code">/center {settings.center || "<number>"}</span></div>
-							<div className="row"><span className="code">/density {settings.density || "<size>"}</span></div>
-							<div className="row"><span className="code">/full</span></div>
-							<div className="row"><span className="code">/image &lt;url&gt;</span></div>
-							{/* <div className="row"><span className="code">/backups {settings.backups}</span></div> */}
-							<div className="row"><span className="code">/backups &lt;path&gt;</span></div>
-							{/* <div className="row"><span className="code">/sync &lt;filepath&gt;</span></div> */}
-						</div>
-						<div className="desc">
-							<div className="row">Set a custom header name</div>
-							<div className="row">Set font-size to number</div>
-							<div className="row">Center layout to any pixel-width</div>
-							<div className="row">UI Density: <span className="code">sm, md, lg</span></div>
-							<div className="row">Full-width layout (default)</div>
-							<div className="row">Top backdrop image</div>
-							<div className="row">Change backup file location</div>
-							{/* <div className="row">Auto-sync file</div> */}
-						</div>
+						<div className="row"><span className="code">/title &lt;text&gt;</span></div>
+						<div className="row desc">Set a custom header name</div>
+						<div className="row"><span className="code">/size {settings.fontSize || "<number>"}</span></div>
+						<div className="row desc">Set font-size to number</div>
+						<div className="row"><span className="code">/center {settings.center || "<number>"}</span></div>
+						<div className="row desc">Center layout to any pixel-width</div>
+						<div className="row"><span className="code">/density {settings.density || "<size>"}</span></div>
+						<div className="row desc">UI Density: <span className="code">sm, md, lg</span></div>
+						<div className="row"><span className="code">/full</span></div>
+						<div className="row desc">Full-width layout (default)</div>
+						<div className="row"><span className="code">/image &lt;url&gt;</span></div>
+						<div className="row desc">Top backdrop image</div>
+						<div className="row"><span className="code">/backups &lt;path&gt;</span></div>
+						<div className="row desc">Change backup file location</div>
 					</div>
+
 				</div>
 			</div>
 
 			<style>{`
+				.hide {
+					display: none;
+				}
+				.group-parent-2 {
+					align-items: flex-start;
+					flex-wrap: wrap;
+				}
+				.group-parent-2 .group {
+					flex: 1 0 285px;
+				}
+				.group {
+					display: grid;
+					grid-template-columns: auto 1fr;
+					// grid-template-columns: fit-content(40%) 1fr;
+					grid-template-rows: auto auto;
+					gap: 0 0.8rem;
+				}
 				.entry-command-options {
 					opacity: 0;
 					position: fixed;
@@ -124,9 +129,12 @@ const EntryCommandOptions = ({
 					font-size: 0.92rem;
 					line-height: 1;
 				}
+				.entry-command-options .code:last-child {
+					margin-right: 0;
+				}
 				.section-heading {
 					margin-bottom: 0.8rem;
-					// padding-top: 1rem;
+					padding-top: 1rem;
 					// border-top: 1px solid rgba(255,255,255,0.2);
 				}
 				.section-heading:first-child {
@@ -138,7 +146,7 @@ const EntryCommandOptions = ({
 					padding-right: 1.7rem;
 				}
 				.entry-command-options-inner .group {
-					margin-bottom: 1rem;
+					// margin-bottom: 1rem;
 				}
 				.entry-command-options-inner .group:last-child {
 					margin-bottom: 0;
